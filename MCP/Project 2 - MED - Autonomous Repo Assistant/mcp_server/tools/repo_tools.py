fa02efd3b85171577688feaf46334c08622e95f0
Om Varma <omvarma1210@gmail.com>
2026-02-25 11:35:54 +0530

    file tracking

--- MCP/Project 2 - MED - Autonomous Repo Assistant/mcp_server/tools/repo_tools.py
+++ MCP/Project 2 - MED - Autonomous Repo Assistant/mcp_server/tools/repo_tools.py
@@ -1,24 +1,16 @@
 from mcp.server.fastmcp import FastMCP
 import subprocess
 import os
+import requests
+from pathlib import Path
+from pprint import pprint
 
 mcp = FastMCP("GH-REPO")
 
 BASE_DIR = "repos"
 
 @mcp.tool()
-def clone_repo(username: str, repo_name: str) -> str:
-    """Clones the github repository locally. 
-    This tool is used when a user gives github repo to work with
-
-    Args:
-        username (str): username or the owner name
-        repo_name (str): the actual repo name of the owner
-
-    Returns:
-        repo_path(str): return the repo path of clones repo
-    """
-
+def clone_repo(username: str, repo_name: str) -> dict:
     clone_url = f"https://github.com/{username}/{repo_name}.git"
     repo_path = os.path.join(BASE_DIR, repo_name)
 
@@ -27,6 +19,79 @@ def clone_repo(username: str, repo_name: str) -> str:
         check=True
     )
 
-    return repo_path
+    root_path = Path(repo_path)
 
-# clone_repo("Om-Varma12", "PaperForge")
+    file_map = {
+        "tree": {},
+        "all_files": [],
+        "by_extension": {},
+        "important_files": []
+    }
+
+    imp_file = ["main.py", "app.py", "main.html", "index.js", "README.md", "package.json", "App.tsx", "main.tsx"]
+
+    for path in root_path.rglob("*"):
+        if not path.is_file():
+            continue
+
+        if any(x in path.parts for x in ["node_modules", ".git", "__pycache__", "dist", "build"]):
+            continue
+
+        if path.suffix in [".lock", ".png", ".jpg", ".svg", ".pack"]:
+            continue
+
+        relative_path = path.relative_to(root_path)
+        relative_str = str(relative_path).replace("\\", "/")
+
+        # all files
+        file_map["all_files"].append(relative_str)
+
+        # by extension
+        ext = path.suffix or "no_extension"
+        file_map["by_extension"].setdefault(ext, []).append(relative_str)
+
+        # important files
+        if path.name in imp_file:
+            file_map["important_files"].append(relative_str)
+
+        # tree build
+        parts = relative_path.parts
+        current = file_map["tree"]
+
+        for part in parts[:-1]:
+            current = current.setdefault(part, {})
+
+        current[parts[-1]] = {}
+
+    return {
+        "repo_path": repo_path,
+        "repo_structure": file_map
+    }
+    
+    
+pprint(clone_repo("Om-Varma12", "CrackEM"))
+
+@mcp.tool()
+def get_repo_information(username: str, repo_name: str) -> dict:
+    """Get the Repository Information
+
+    Args:
+        username (str): GitHub username
+        repo_name (str): Repository name
+
+    Returns:
+        dict: information of repository in json format
+    """
+    
+    response = requests.get(f"https://api.github.com/repos/{username}/{repo_name}")
+    info = response.json()
+    
+
+    return {
+        "description": info['description'],
+        "language": info['language'],
+        "created_at": info['created_at'],
+        "lasted_updated_at": info['updated_at']
+    }
+    
+# pprint(get_repo_information("Om-Varma12", "PaperForge"))
