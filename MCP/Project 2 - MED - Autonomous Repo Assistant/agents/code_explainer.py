b48bf982de2cab7f333673b05d5613c2257c0ffc
Om Varma <omvarma1210@gmail.com>
2026-03-01 19:50:16 +0530

    sub code explainer agents

--- MCP/Project 2 - MED - Autonomous Repo Assistant/agents/code_explainer.py
+++ MCP/Project 2 - MED - Autonomous Repo Assistant/agents/code_explainer.py
@@ -1,52 +1,22 @@
-import asyncio
 from langchain_groq import ChatGroq
-from langchain_mcp_adapters.client import MultiServerMCPClient
 from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGroq(model="openai/gpt-oss-20b", temperature=0)
 
-client = MultiServerMCPClient({
-    "repo": {
-        "command": "python",
-        "args": ["-u", "mcp_server/server.py"],
-        "transport": "stdio",
-    }
-})
+with open("prompts/code_explainer.txt") as f:
+    PROMPT = f.read()
 
+async def _run(user_query: str, file_name: str, file_summary: str):
+    prompt = (
+        PROMPT
+        .replace("{user_query}", user_query)
+        .replace("{file_name}", file_name)
+        .replace("{file_summary}", file_summary)
+    )
 
-async def _run(question, files, repo_summary, repo_name):
-    prompt = f"""
-        Explain code shortly.
-
-        Question: {question}
-        Files: {files}
-        Repo Summary: {repo_summary}
-        Repo Name: {repo_name}
-    """
-
-    tools = await client.get_tools()
-    model_with_tools = llm.bind_tools(tools)
-
-    response = await model_with_tools.ainvoke(prompt)
-
-    if response.tool_calls:
-        tool_call = response.tool_calls[0]
-        tool = {t.name: t for t in tools}[tool_call["name"]]
-        tool_result = await tool.ainvoke(tool_call["args"])
-
-        final = await model_with_tools.ainvoke([
-            response,
-            {
-                "role": "tool",
-                "tool_call_id": tool_call["id"],
-                "content": tool_result
-            }
-        ])
-        return final.content
-
+    response = await llm.ainvoke(prompt)
     return response.content
 
-
-async def run(question, files, repo_summary, repo_name):
-    return await _run(question, files, repo_summary, repo_name)
+async def run(user_query: str, file_name: str, file_summary: str):
+    return await _run(user_query, file_name, file_summary)
